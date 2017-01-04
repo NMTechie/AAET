18b26d6abd497781e09431f8bb11d971fdbf6460
N R Mitra <nileshranjan.mitra@yahoo.in>
2017-01-04 14:45:12 +0530

    base version

---
+++ Artefacts/UI/Scripts/Views/CommonUtil.js
@@ -0,0 +1,23 @@
+﻿(function ($) {
+
+    $.fn.ClearText = function () {
+        $(this).each(function () {
+            $(this).val('');
+        });
+    };
+    
+}(jQuery));
+
+/*
+This approach also work but not aligned with modern Jquery 
+function ChangeFormAttribute (str)
+{
+    alert('Hi');
+    $('form').attr('action', str);
+}*/
+
+function ButtonPostActionTo(clickedButton, urlToPost) {
+    $(clickedButton).closest('form').attr('action', urlToPost);
+}
+
+
---
+++ Artefacts/UI/Scripts/Views/ShowResult.js
@@ -0,0 +1,59 @@
+﻿(function () {
+
+    $(document).ready(function () {
+        $('#resultSearchGrid').DataTable(
+        {
+            "serverSide": true,
+            "processing": true,
+            "deferRender": true,
+            "sDom": '<"top"l>rt<"bottom"ip><"clear">',
+            "lengthMenu": [[10, 20, 50, 100, -1], [10, 20, 50, 100, "All"]],
+            "language": {
+                "info": "Showing page _PAGE_ of _PAGES_",
+                "infoEmpty": "No records found for this filter criteria",
+                "infoFiltered": ""
+            },
+            "order": [[0, 'asc']],
+            "rowId": 'MessageId',
+            "ajax": {
+                "url": '/ConfigurationUIGridBehaviour',
+                "data": function (d) {
+                    return $.extend({}, d, {
+                        "searchMessageId": $('#txtMessageId').val(),
+                        "searchMessageBody": $('#txtMessageBody').val(),
+                        "searchMessageReceiveTime": $('#txtMessageReceiveTime').val(),
+                        "searchStudentName": $('#txtStudentName').val()
+                    });
+                },
+                "dataSrc": 'ResultSet'
+            },
+            "sPaginationType": 'full_numbers',
+            "columnDefs": [
+                     { title: 'Message Id', targets: 0, data: 'MessageId', name: "MessageId" },
+                     { title: 'Student Name', targets: 1, data: 'StudentName', name: "StudentName" },
+                     { title: 'Message Body', targets: 2, data: 'MessageBody', name: "MessageBody" },
+                     { title: 'Message ReceiveTime', targets: 3, data: 'MessageReceiveTime', name: "MessageReceiveTime" }                     
+            ]
+        }
+        );
+    });
+
+    btnSearch_Click = function () {
+        filterSearchColumn();
+    };
+
+    btnClearSearch_Click = function () {
+        clearSearchParams();
+        filterSearchColumn();
+    };
+
+    function filterSearchColumn() {
+        $('#resultSearchGrid').DataTable().draw();
+    };
+
+    function clearSearchParams() {
+        $('#txtMessageId,#txtStudentName,#txtMessageBody,#txtMessageReceiveTime').ClearText();
+    };
+
+}(jQuery));
+
