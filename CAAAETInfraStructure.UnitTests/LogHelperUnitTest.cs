d61e52090af72d376e6717dec01bfb0dd62b0201
N R Mitra <nileshranjan.mitra@yahoo.in>
2017-01-04 14:51:36 +0530

    base version

---
+++ CAAAETInfraStructure.UnitTests/LogHelperUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CAAAETInfraStructure.Logging;
+using CAAAETCommon.CustomException;
+
+namespace CAAAETInfraStructure.UnitTests
+{
+    [TestClass]    
+    public class LogHelperUnitTest
+    {
+        [TestMethod]
+        [TestCategory("UnitTestLogHelper")]
+        [Priority(3)]
+        public void WriteInfo()
+        {
+            string information = "TESTING INFORMATION WRITTING";
+            Assert.AreEqual(LogHelper.GetLogger().WriteInfo(information), true);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTestLogHelper")]
+        [Priority(3)]
+        public void WriteWarning()
+        {
+            string information = "TESTING WARNING WRITTING";
+            Assert.AreEqual(LogHelper.GetLogger().WriteInfo(information), true);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTestLogHelper")]
+        [Priority(3)]
+        public void WriteException()
+        {
+            Exception ex = new Exception("TESTING Exception WRITTING");
+            Assert.AreEqual(LogHelper.GetLogger().WriteException(ex), true);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTestLogHelper")]
+        [Priority(3)]
+        public void WriteBusinessException()
+        {
+            CAAAETException ex = new CAAAETException("TESTING BUSINESS EXCEPTION WRITTING");
+            Guid errorId;
+            if (!Guid.TryParse(LogHelper.GetLogger().WriteBusinessException(ex),out errorId))
+            {
+                Assert.Fail("Return errorid is not a GUID. The exception has not been logged");
+            }      
+        }
+    }
+}
