2bc3ef830b0c3e17c3785abdefd8144545e662da
N R Mitra <nileshranjan.mitra@yahoo.in>
2017-01-04 14:44:21 +0530

    base version

---
+++ ApplicationBuildValidator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationBuildValidator.UserAccessControlXML;
+
+namespace ApplicationBuildValidator
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args!=null && args.Length > 0 && args[0].Trim().ToUpper().Equals("XML"))
+            {
+                UserAccessControlValidator validateXMLSchema = new UserAccessControlValidator();
+                return validateXMLSchema.ValidateMenuSchema();
+            }
+            return 1;
+        }
+    }
+}
---
+++ ApplicationBuildValidator/UserAccessControlXML/UserAccessControlValidator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Linq;
+using System.Diagnostics;
+
+namespace ApplicationBuildValidator.UserAccessControlXML
+{
+    public class UserAccessControlValidator
+    {
+        public int ValidateMenuSchema()
+        {
+            int retVal = 1;
+            try
+            {                
+                XDocument menuCntrlXML = XDocument.Load("UserAccessControl1.xml");
+                String msg = string.Empty;
+                var roles = menuCntrlXML.Descendants("role");
+
+                foreach (var role in roles)
+                {
+                    var menus = role.Elements();
+                    foreach (var menu in menus)
+                    {
+                        ValidateMenu(menu);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                retVal = ex.Message.Length;
+            }
+            return retVal;
+        }
+        private void ValidateMenu(XElement menu)
+        {
+            bool isLeaf = Convert.ToBoolean(menu.Attribute("IsLeaf").Value);
+            var menuText = menu.Attribute("MenuText");
+            if (menuText == null)
+            {
+                throw new Exception("Menu defined without text");
+            }
+            if (isLeaf)
+            {
+                var actionName = menu.Attribute("ActionName");
+                var controllerName = menu.Attribute("ControllerName");
+                var screen = menu.Attribute("Screen");
+
+                if (actionName == null || controllerName == null)
+                {
+                    throw new Exception("Menu created without web link");
+                }
+            }
+            else
+            {
+                var subMenus = menu.Elements();
+                if (subMenus.Count() == 0)
+                {
+                    throw new Exception("No submenu defined under non leaf menu");
+                }
+                foreach (var submenu in subMenus)
+                {
+                    ValidateMenu(submenu);
+                }
+            }
+        }
+    }
+}
