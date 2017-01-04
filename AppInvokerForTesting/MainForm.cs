b1d95fa9df53b4e3cd11a4522f918860b5080e5d
N R Mitra <nileshranjan.mitra@yahoo.in>
2017-01-04 14:41:47 +0530

    base version check in

---
+++ AppInvokerForTesting/MainForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CAAAETInfraStructure;
+using CAAAETInfraStructure.DataBase;
+using CAAAETInfraStructure.Logging;
+using CAAAETCommon.CustomException;
+using CAAAETCommon.Enums;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace AppInvokerForTesting
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            try
+            {
+                InitializeComponent();
+                //LogHelper.GetLogger().WriteInfo("Hi");
+                //LogHelper.GetLogger().WriteWarning("Warning");
+
+                ////Exception ex = new Exception("Error");
+                ////throw ex;
+                //CAAAETException ex1 = new CAAAETException("This is business exception");
+                //throw ex1;
+                
+
+                DbParameter p = CAAAETDataBaseFactory.GetDataBase(CAAAETApplicationDBEnums.SQL).CreateParameter("N", DbType.Int16, ParameterDirection.Input);
+                var obj = CAAAETDataBaseFactory.GetDataBase(CAAAETApplicationDBEnums.SQL).ExecuteStoredProc("Hello",p);
+                
+                
+            }
+            catch (CAAAETException ex)
+            {
+                LogHelper.GetLogger().WriteBusinessException(ex);
+            }
+            catch (Exception ex)
+            {
+                LogHelper.GetLogger().WriteException(ex);
+            }
+        }
+    }
+}
